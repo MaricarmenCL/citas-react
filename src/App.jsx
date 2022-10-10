b8703d7aed946764dac9d575642103debf38b70f
Maricarmen Cervantes <pamen1998@hotmail.com>
2022-10-10 16:59:29 -0600

    H1 y H2
    se creo h1 y h2 para citas react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//nucleo de mi proyecto
+//une los componentes
+import Header from "./componentes/header"
+import ListadoPaciente from "./componentes/listadopaciente"
+
+
+function App() {
+  
+
+  return (
+    <>
+<Header/>
+<ListadoPaciente/>
+    </>
+  )
+}
+
+export default App
---
+++ src/componentes/header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+
+    return(
+        <h1>Primer Componente: header</h1>
+        
+    )
+}
+
+
+
+export default Header
